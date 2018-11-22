f3646379ce5c52c3ddb8c03b02f3d44cb4f14467
Nathaniel Pather <nathanielpather@192-168-1-6.tpgi.com.au>
2018-11-22 11:39:04 +1000

    Initial Commit

---
+++ WorkoutApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkoutApp
+//
+//  Created by Nathaniel Pather on 22/11/18.
+//  Copyright © 2018 Nathaniel Pather. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
